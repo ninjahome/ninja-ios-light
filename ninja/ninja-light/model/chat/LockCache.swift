287489d1feb4dba772b8c76934b0c2ca5cefb6f9
akatuki7 <akatuki7@hotmail.com>
2021-11-01 11:09:53 +0800

    cache lock

--- ninja/ninja-light/model/chat/LockCache.swift
+++ ninja/ninja-light/model/chat/LockCache.swift
@@ -8,69 +8,68 @@
 import Foundation
 
 class LockCache<T> : NSObject {
-    lazy var rwLock: pthread_rwlock_t = {
-        var lock = pthread_rwlock_t()
-        pthread_rwlock_init(&lock, nil)
-        return lock
-    }()
-    
+    var rwLock = pthread_rwlock_t()
     var contents: [String: T] = [:]
+
+    override init() {
+        pthread_rwlock_init(&rwLock, nil)
+    }
     
-    deinit{
+    deinit {
         pthread_rwlock_destroy(&rwLock)
     }
-        
+
     func getValues() -> [T] {
         pthread_rwlock_rdlock(&rwLock)
         defer {
             pthread_rwlock_unlock(&rwLock)
         }
-        
+
         return Array(contents.values)
     }
-    
+
     func get(idStr: String) -> T? {
-        
+
         pthread_rwlock_rdlock(&rwLock)
         defer {
             pthread_rwlock_unlock(&rwLock)
         }
-        
+
         return contents[idStr]
-        
+
     }
-    
+
     func setOrAdd(idStr: String, item: T?) {
-        
+
         pthread_rwlock_wrlock(&rwLock)
         defer {
             pthread_rwlock_unlock(&rwLock)
         }
-        
+
         if let i = item {
             contents.updateValue(i, forKey: idStr)
         }
-        
+
     }
-    
+
     func delete(idStr: String) {
-        
+
         pthread_rwlock_wrlock(&rwLock)
         defer {
             pthread_rwlock_unlock(&rwLock)
         }
-        
+
         contents.removeValue(forKey: idStr)
     }
-    
+
     func deleteAll() {
-        
+
         pthread_rwlock_wrlock(&rwLock)
         defer {
             pthread_rwlock_unlock(&rwLock)
         }
-        
+
         contents.removeAll(keepingCapacity: true)
     }
-    
+
 }
