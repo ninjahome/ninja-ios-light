c57666b72778662bdb4319d7fc7097e5b7e9879d
hopwesley <ws@sinoc.io>
2022-01-30 14:54:03 +0800

    expire ui

--- ninja/ninja-light/wallet/transfer/ConfirmTransferViewController.swift
+++ ninja/ninja-light/wallet/transfer/ConfirmTransferViewController.swift
@@ -8,50 +8,51 @@
 import UIKit
 
 class ConfirmTransferViewController: UIViewController {
-
-    @IBOutlet weak var transferAddr: UILabel!
-    @IBOutlet weak var expire: UILabel!
-    @IBOutlet weak var inputTransferDays: UITextField!
-    @IBOutlet weak var confirmBtn: UIButton!
-    
-    var transAddress: String?
-    var expireDays: Double?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        transferAddr.text = transAddress
-        expireDays = AgentService.shared.expireDays
-        expire.text = "剩余激活天数\(expireDays ?? 0)天"
         
-        self.hideKeyboardWhenTappedAround()
-    }
-    
-    @IBAction func transferAll(_ sender: UIButton) {
-        inputTransferDays.text = String(expireDays ??  0)
-    }
-    
-    @IBAction func confirmTransfer(_ sender: UIButton) {
-        guard let days = inputTransferDays.text, let dayInt = Int(days) else {
-            return
+        @IBOutlet weak var transferAddr: UILabel!
+        @IBOutlet weak var expire: UILabel!
+        @IBOutlet weak var inputTransferDays: UITextField!
+        @IBOutlet weak var confirmBtn: UIButton!
+        
+        var transAddress: String?
+        
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                transferAddr.text = transAddress
+                let expireDays = AgentService.shared.expireDays
+                expire.text = String(format: "剩余激活天数 %.2f 天", expireDays)
+                
+                self.hideKeyboardWhenTappedAround()
         }
         
-        guard let addr = transAddress else {
-            return
+        @IBAction func transferAll(_ sender: UIButton) {
+                
+                let expireDays = AgentService.shared.expireDays
+                inputTransferDays.text = String(expireDays )
         }
         
-        if AgentService.shared.transferLicense(to: addr, days: dayInt) {
-            
-            self.toastMessage(title: "success")
-            self.navigationController?.popToRootViewController(animated: true)
-        } else {
-            
-            self.toastMessage(title: "faild")
+        @IBAction func confirmTransfer(_ sender: UIButton) {
+                guard let days = inputTransferDays.text, let dayInt = Int(days) else {
+                        return
+                }
+                
+                guard let addr = transAddress else {
+                        return
+                }
+                
+                if AgentService.shared.transferLicense(to: addr, days: dayInt) {
+                        
+                        self.toastMessage(title: "success")
+                        self.navigationController?.popToRootViewController(animated: true)
+                } else {
+                        
+                        self.toastMessage(title: "faild")
+                }
+                
         }
         
-    }
-    
-    @IBAction func returnItem(_ sender: UIBarButtonItem) {
-        self.navigationController?.popViewController(animated: true)
-    }
-
+        @IBAction func returnItem(_ sender: UIBarButtonItem) {
+                self.navigationController?.popViewController(animated: true)
+        }
+        
 }
