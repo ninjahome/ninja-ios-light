bce6354b56da9e338ad0ea5213dccf0795210207
hopwesley <ws@sinoc.io>
2022-02-01 01:38:19 +0800

    save

--- ninja/ninja-light/message/messageCells/MessageTableViewCell.swift
+++ ninja/ninja-light/message/messageCells/MessageTableViewCell.swift
@@ -8,37 +8,32 @@
 import UIKit
 
 class MessageTableViewCell: UITableViewCell {
-
+        
         @IBOutlet weak var msgBackgroundView: UIImageView!
-
+        
         @IBOutlet weak var msgLabel: UITextView!
         @IBOutlet weak var avatar: AvatarButton!
         @IBOutlet weak var nickname: UILabel!
         @IBOutlet weak var time: UILabel!
-
+        
         @IBOutlet weak var spinner: UIActivityIndicatorView?
-
+        
         override func prepareForReuse() {
                 super.prepareForReuse()
                 spinner?.stopAnimating()
         }
-    
+        
         override func awakeFromNib() {
                 super.awakeFromNib()
         }
-
+        
         override func setSelected(_ selected: Bool, animated: Bool) {
                 super.setSelected(selected, animated: animated)
         }
-
-    
-        func updateMessageCell (by message: MessageItem) {
-//        msgBackgroundView.layer.cornerRadius = 8
-//        msgBackgroundView.clipsToBounds = true
-//
-//        trailingConstrain = msgBackgroundView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20)
-//        leadingConstrain = msgBackgroundView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20)
         
+        
+        func updateMessageCell (by message: MessageItem) {
+                
                 if let msgText = message.payload as? String {
                         msgLabel.text = msgText
                 }
@@ -46,7 +41,7 @@ class MessageTableViewCell: UITableViewCell {
                 guard let from = message.from else {
                         return
                 }
-                //message bubble
+                
                 if message.isOut {
                         switch message.status {
                         case .faild:
@@ -56,7 +51,7 @@ class MessageTableViewCell: UITableViewCell {
                         default:
                                 spinner?.stopAnimating()
                         }
-
+                        
                         let img = UIImage(named: "white")?.resizableImage(withCapInsets: UIEdgeInsets(top: 20, left: 12, bottom: 10, right: 12), resizingMode: .stretch)
                         msgBackgroundView.image = img
                         avatar.type = AvatarButtonType.wallet
@@ -65,15 +60,14 @@ class MessageTableViewCell: UITableViewCell {
                 } else {
                         let img = UIImage(named: "babycolor")?.resizableImage(withCapInsets: UIEdgeInsets(top: 20, left: 12, bottom: 10, right: 12), resizingMode: .stretch)
                         msgBackgroundView.image = img
-                    
+                        
                         avatar.type = AvatarButtonType.contact
                         
                         let contactData = CombineConntact.cache[from]
                         avatar.avaInfo = AvatarInfo.init(id: from, avaData: contactData?.account?.Avatar)
-                        nickname.text = contactData?.GetNickName() ?? contactData?.peerID                        
+                        nickname.text = contactData?.GetNickName() ?? contactData?.peerID
                 }
                 
                 time.text = formatMsgTimeStamp(by: message.timeStamp)
         }
-
 }
