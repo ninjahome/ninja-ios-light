74b677cc45c0816faff8149ccbff828329647aae
hopwesley <ws@sinoc.io>
2022-02-03 21:54:29 +0800

    resend message logic

--- ninja/ninja-light/message/messageCells/MessageTableViewCell.swift
+++ ninja/ninja-light/message/messageCells/MessageTableViewCell.swift
@@ -10,17 +10,17 @@ import UIKit
 class MessageTableViewCell: UITableViewCell {
         
         @IBOutlet weak var msgBackgroundView: UIImageView!
-        
         @IBOutlet weak var msgLabel: UITextView!
         @IBOutlet weak var avatar: AvatarButton!
         @IBOutlet weak var nickname: UILabel!
         @IBOutlet weak var time: UILabel!
-        
+        @IBOutlet weak var retry: UIButton?
         @IBOutlet weak var spinner: UIActivityIndicatorView?
         
         let inMsgImg = UIImage(named: "white")?.resizableImage(withCapInsets: UIEdgeInsets(top: 20, left: 12, bottom: 10, right: 12), resizingMode: .stretch)
         let ourImg = UIImage(named: "babycolor")?.resizableImage(withCapInsets: UIEdgeInsets(top: 20, left: 12, bottom: 10, right: 12), resizingMode: .stretch)
         
+        var curMsg:MessageItem?
         override func prepareForReuse() {
                 super.prepareForReuse()
                 spinner?.stopAnimating()
@@ -34,9 +34,22 @@ class MessageTableViewCell: UITableViewCell {
                 super.setSelected(selected, animated: animated)
         }
         
+        @IBAction func resendFailedMsg(_ sender: Any) {
+                guard let msg = self.curMsg else{
+                        print("------>>>no valid msg in current cell")
+                        return
+                }
+                msg.status = .sending
+                spinner?.startAnimating()
+                retry?.isHidden = true
+                if let err = WebsocketSrv.shared.SendMessage(msg: msg){
+                        print("------>>> retry failed:=>", err)
+                        msg.status = .faild
+                }
+        }
         
         func updateMessageCell (by message: MessageItem) {
-                
+                self.curMsg = message
                 let from = message.from
                 guard let msgText = message.payload as? txtMsg else{
                         msgLabel.text = "Invalid Text MSG"
@@ -47,6 +60,7 @@ class MessageTableViewCell: UITableViewCell {
                         switch message.status {
                         case .faild:
                                 spinner?.stopAnimating()
+                                retry?.isHidden = false
                         case .sending:
                                 spinner?.startAnimating()
                         default:
