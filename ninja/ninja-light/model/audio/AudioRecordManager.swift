6f4e8f10fce855ec837642d3c801e37a96cc1bad
hopwesley <ws@sinoc.io>
2022-02-13 10:59:53 +0800

    audio sample ratio

--- ninja/ninja-light/model/audio/AudioRecordManager.swift
+++ ninja/ninja-light/model/audio/AudioRecordManager.swift
@@ -23,13 +23,13 @@ class AudioRecordManager:NSObject {
         weak var delegate: RecordAudioDelegate?
         var currntUrl = FileManager.TmpDirectory().appendingPathComponent(TempWavRecordPath)
         
-        let recoredSetting = [AVSampleRateKey: NSNumber(8000.0),
-                          AVEncoderBitRateKey: NSNumber(8000.0),
+        let recoredSetting = [AVSampleRateKey: NSNumber(22050.0),
+                          AVEncoderBitRateKey: NSNumber(22050.0),
                                 AVFormatIDKey: NSNumber(value: kAudioFormatMPEG4AAC),
                         AVNumberOfChannelsKey: NSNumber(1),
                            AVAudioFileTypeKey:kAudioFileM4AType,
                      AVEncoderBitDepthHintKey:16,
-                     AVEncoderAudioQualityKey: NSNumber(value: AVAudioQuality.high.rawValue)] as [String : Any]
+                     AVEncoderAudioQualityKey: NSNumber(value: AVAudioQuality.medium.rawValue)] as [String : Any]
         
         
         func startRecord() -> Error?{
